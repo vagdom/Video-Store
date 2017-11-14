e3d27fccaf2f2d2990c67b9c8be927f1abfcdf86
vagdom <vagdom@hotmail.com>
2017-11-14 16:25:58 -0500

    Update videoType.h

--- videoType.h
+++ videoType.h
@@ -44,6 +44,60 @@ public:
   //Function to print the details of a movie.
   //Postcondition: The title of the movie, stars, director, and
   //               so on are displayed on the screen.
-
+  
+  bool checkTitle(string title);
+  //Function to check whether the title is the same as the title
+  //of the video.
+  //Postcondition: Returns true if the title is the same as
+  //               the title of the video; otherwise, returns
+  //               false.
+  
+  void updateInStock(int num);
+  //function to increment the copies in stock by
+  //adding the value of the parameter num.
+  //Postcondition: copiesInStock = copiesInStock + num.
+  
+  void setCopiesInStock(int num);
+  //Function to set the number of copies in stock.
+  //Postcondition: copiesInStock = num.
+  
+  string getTitle();
+  //Function to return the title of the video.
+  //Postcondition: The title of the video is returned.
+  
+  videoType(string title = "", string star1 = "",
+            string star2 = "", string producer = "",
+            string director = "", string productionCo = "",
+            int setInStock = 0);  
+  //Constructor
+  //Private data members are set according to the incoming
+  //parameters. If no values are specified, the default
+  //values are assigned.
+  //Postcondition: videoTitle = title
+  //               movieStar1 = star1
+  //               movieStar2 = star2
+  //               movieDirector = director
+  //               movieProductionCo = productionCo
+  //               copiesInStock = setInStock
+  
+  //Overload the relational operators.
+  bool operator == ( const videoType& ) const;
+  bool operator != ( const videoType& ) const;
+  
+private:
+  string videoTitle;                //variable to store the name of the
+                                    //movie
+  string movieStar1;                //variable to store the name of the
+                                    //movie star1
+  string movieStar2;                //variable to store the name of the
+                                    //movie star2
+  string movieProducer;             //variable to store the name of the
+                                    //movie producer
+  string movieDirector;             //variable to store the name of the
+                                    //movie director
+  string movieProductionCo;         //variable to store the name of the
+                                    //movie production company
+  int   copiesInStock;              //variable to store the number of copies
+                                    //in stock
 };
 #endif
