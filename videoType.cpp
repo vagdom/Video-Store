767bf2df42f49595b29deef87003368177c04c63
vagdom <vagdom@hotmail.com>
2017-11-15 13:37:23 -0500

    Create videoType.cpp

---
+++ videoType.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+
+#include "videoType.h"
+
+using namespace std;
+
+void videoType::setVideoInfo(string title, string star1,
+                             string star2, string producer,
+                             string director,
+                             string productionCo, int setInStock)
+{
+    videoTitle = title;
+    movieStar1 = star1;
+    movieStar2 = star2;
+    movieProducer = producer;
+    movieDirector = director;
+    movieProductionCo = productionCo;
+    copiesInStock = setInSctock;
+}
+
+void videoType::checkOut()
+{
+    if(getNoOfCopiesInStock() > 0)
+        copiesInStock--;
+    else
+        cout << "Currently out of stock " << endl;
+}
+
+void videoType::checkIn()
+{
+    copiesInStock++;
+}
+
+int videoType::getNoOfCopiesInStock() const
+{
+    return copiesInStock;
+}
+
+void videoType::printTile() const
+{
+    cout << "Video Title : " << videoTitle << endl;
+}
+
+void videoType::printInfo() const
+{
+    cout << "Video Title : << videoTitle << endl;
+    cout << "Stars : " << movieStar1 << " and " << movieStar2 << endl;
+    cout << "Producer : " << movieProducer << endl;
+    cout << "Director : " << movieDirector << endl;
+    cout << "Production : " << movieProductionCo << endl;
+    cout << "Copies in stock : " << copiesInStock << endl;
+}
+
+bool videoType::checkTitle(strilg title)
+{
+    return(videoTitle == tittle);
+}
+
+void videoType::updateInStock(int num)
+{
+    copiesInStock += num;
+}
+
+void videoType::setCopiesInStock(int num)
+{
+    copiesInStock = num;
+}
+
+string videoType::getTitle()
+{
+    return videoTitle;
+}
+ 
+videoType::videoType((string title, string star1,
+                             string star2, string producer,
+                             string director,
+                             string productionCo, int setInStock)
+{
+    setVideoInfo(title, star1, star2, producer, director,
+                 productionCo, setInStock);
+}
+
+bool videoType::operator == (const videoType& other) const
+{
+    return ( videoTitle == other.videoTitle);
+}
+
+
+    
