838af82050f301b73db3f46696335ddecbcf8f62
vagdom <vagdom@hotmail.com>
2017-11-16 13:05:23 -0500

    Update linkedListType.cpp

--- linkedListType.cpp
+++ linkedListType.cpp
@@ -13,5 +13,33 @@ class linkedListType
   //Overload the stream insertion operator
   friend ostream& operator << (ostream&, const linkedListType<Type>&);
 public:
-  const linkedListType<Type>& operator = (const 
+  //Overload the assign operator
+  const linkedListType<Type>& operator = (const linkedListType<Type>&);
+  
+  //Function to initialize the list to an empty state.
+  //Postcondition: first = NULL; last = NULL;
+  //               count = 0
+  void initializeList();
+  
+  //Function to determine whether the list is empty.
+  //Postcondition: Returns true if the list is empty;
+  //               otherwise, returns false.
+  bool isEmptyList();
+  
+  //Function to return the number of nodes in the
+  //list.
+  //Postcondition: The value of count is returned.
+  int length();
+  
+  //Function to return the first element of the list.
+  //Precondition: The list must exist and must not be empty.
+  //Postcondition: If the list is empty, then the
+  //               program terminates; otherwise, the first
+  //               element of the list is returned.
+  Type front();
+  
+  //Function to delete all nodes from the list.
+  //Postcondition: first = NULL; last = NULL;  
+  //               count = 0
+  void destroyList();
   
