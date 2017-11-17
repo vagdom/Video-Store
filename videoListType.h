168f5be8a3ddb6b29e2dede602213fb0fe95b159
vagdom <vagdom@hotmail.com>
2017-11-16 20:36:54 -0500

    Update videoListType.h

--- videoListType.h
+++ videoListType.h
@@ -8,6 +8,45 @@ using namespace std;
 
 class videoListType: public linkedListType<videoType>
 {
-
-
+public:
+  //Function to search the list to see whether a
+  //particular title, specified by parameter title,
+  //is in the store.
+  //Postcondition: Returns true if the title is found;
+  //               otherwise, returns false
+  bool videoSearch(string vTitle);
+  
+  //Function to return true if at least one copy of a 
+  //particular video is in the store.
+  bool isVideoAvailable(string vTitle);
+  
+  //Function to check out a video, that is, rent a video.
+  //Postcondition: copiesInStock is decremented by one.
+  void videoCheckOut(string vTitle);
+  
+  //Function to check in a video returned by a customer.
+  //Postcondition: copiesInStock is incremented by one.
+  void videoCheckIn(string vTitle);
+  
+  //Function to determine whether a particular video is in
+  //the store.
+  //Postcondition: Returns true if the video is the 
+  //               same as vTitle; otherwise, returns false.
+  bool videoCheckTitle(string vTitle);
+  
+  //Function to update the number of copies of a video
+  //by adding the value of the parameter num. The
+  //parameter vTitle specifies the name of the video for
+  //which the number of copies is to be updated.
+  //Postcondition: copiesInStock = copiesInStock + num.
+  void videoUpdateInStock(string vTitle, int num);
+  
+  //Function to reset the number of copies of a video.
+  //The parameter vTitle specifies the name of the video
+  //for which the number of copies is to be reset; the
+  //parameter num specifies the number of copies.
+  //Postcondition: copiesInStock = num
+  void setcopiesInStock(string vTitle, int num)
+  
+  //Function to print the titles of all movies.
 };
