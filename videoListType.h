67f41063cb76953e8b7e2e32072aa8c0b20d8cc2
vagdom <vagdom@hotmail.com>
2017-11-16 19:46:32 -0500

    Create videoListType.h

---
+++ videoListType.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+#include "linkedListType.h"
+#include "videoType.h"
+
+using namespace std;
+
+class videoListType: public linkedListType<videoType>
+{
+
+
+};
