12f3262beebe74dfb04222815fa4256f7c8c1e64
vagdom <vagdom@hotmail.com>
2017-11-24 02:14:43 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,4 +36,67 @@ int main()
   cin >> choice;
   cin.get(ch);
   cout << endl;
+  
+  //Process the request
+  while(choice != 9)
+  {
+   switch(choice)
+   {
+     case 1:
+     case 2:
+     case 3:
+     case 4:
+     case 5:
+     case 6:
+     default: 
+       
+   }
+    
+  }
+  
+  return 0;
+}
+
+void createVideoList(ifstream& infile, videoListType& videoList)
+{
+  string Title;
+  string Star1;
+  string Star2;
+  string Producer;
+  string Director;
+  string ProductionCo;
+  char ch;
+  int InStock;
+  
+  videoType newVideo;
+  
+  getline(infile, Title);
+  while(infile)
+  {
+    getline(infile, Star1);
+    getline(infile, Star2);
+    getline(infile, Producer);
+    getline(infile, Director);
+    getline(infile, ProductionCo);
+    infile >> InStock;
+    infile.get(ch);
+    
+    newVideo.setVideoInfo(Title, Start1, Star2, Producer,
+                          Director, ProductionCo, InStock);
+    videoList.insertFirst(newVideo);
+    
+    getline(infile, Title);
+  }
+}
+
+void displayMenu()
+{
+  cout << "Select one of the following " << endl;
+  cout << "1: To check whether a particular video is in the store " << endl;
+  cout << "2: To check out a video " << endl;
+  cout << "3: To check in a video  " << endl;
+  cout << "4: To check whether a particular title is in stock " << endl;
+  cout << "5: To print the titles of all the videos " << endl;
+  cout << "6: To print a list of all the videos " << endl;
+  cout << "9: To exit" << endl;
 }
